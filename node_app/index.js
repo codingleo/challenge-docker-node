26de0c082a517d9b632c58ebb7ff77bff6d42d31
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2023-09-17 22:15:00 -0300

    able to add people on the list

--- node_app/index.js
+++ node_app/index.js
@@ -1,19 +1,39 @@
+import { faker } from '@faker-js/faker'
+import ejs from 'ejs'
 import express from 'express'
 import mysql from 'mysql'
 
 const config = {
   host: 'db',
-  port: 3306,
-  use: 'admin',
-  password: 'admin'
+  user: 'admin',
+  password: 'admin',
+  database: 'nodedb'
 }
 
-const connection = mysql(config)
+const connection = mysql.createConnection(config) 
+
+connection.query('CREATE DATABASE IF NOT EXISTS nodedb')
+connection.query('USE nodedb')
+connection.query('CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))')
+connection.query(`INSERT INTO people(name) values("${faker.person.fullName()}")`)
 
 const app = express()
 
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+  connection.query('SELECT * FROM people', (err, rows) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const people = rows.map(row => ({ id: row.id, name: row.name}))
+    res.render('index.ejs', { people })
+    return
+  })
 })
 
 app.listen(3000, () => {
